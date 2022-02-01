22fd821768dfb23f1bf19b04a621e9c444bba464
dcuevas24 <ivandaniel1224@gmail.com>
2022-02-01 13:15:54 -0500

    Refactorizar codigo

--- challenges/challenge_02/main.js
+++ challenges/challenge_02/main.js
@@ -2,22 +2,22 @@ const letter = "bici coche balón _playstation bici coche peluche";
 
 const stringToArray = letter.split(" ");
 
-const counterGifts = stringToArray
-  .filter((gift) => {
-    const expReg = /_/g;
+const filterGifts = stringToArray.filter((gift) => {
+  const expReg = /_/g;
 
-    if (!expReg.test(gift)) {
-      return gift;
-    }
-  })
-  .reduce((obj, gift) => {
-    if (obj[gift]) {
-      obj[gift] += 1;
-    } else {
-      obj[gift] = 1;
-    }
+  if (!expReg.test(gift)) {
+    return gift;
+  }
+});
 
-    return obj;
-  }, {});
+const counterGifts = filterGifts.reduce((obj, gift) => {
+  if (obj[gift]) {
+    obj[gift] += 1;
+  } else {
+    obj[gift] = 1;
+  }
+
+  return obj;
+}, {});
 
 console.log(counterGifts);
